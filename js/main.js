63793de34779d75fa2e5c9059432d4228e28b2ba
JDPATEL-1410 <pateljili9004@gmail.com>
2025-08-23 11:46:04 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -347,11 +347,5 @@ tabs.forEach(tab => {
   });
 });
 
-// Mobile Menu
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
 
